613f71da63ac66d372f71caa6c8e118f2c4afa14
Chi Feng <email.chi.feng@gmail.com>
2015-11-20 18:54:53 -0500

    Update DPSolver.h

--- src/DPSolver.h
+++ src/DPSolver.h
@@ -8,19 +8,21 @@
 #include "State.h"
 #include "Model.h"
 
-class ValueFunction
+class DPSolver
 {
 
 public:
-
+  
+  std::shared_ptr<Model> model;
+  
   int stages;
 
-  DPSolver(const int stages) : stages(stages);
+  DPSolver(const int stages,std::shared_ptr<const Model> model) : stages(stages), model(model) { }
 
   std::vector<std::shared_ptr<ValueFunction>> valueFunctions;
   
   // returns optimal control and value 
-  std::pair<double, double> GetOptimalControl(std::shared_ptr<const State> state, std::shared_ptr<const ValueFunction> nextValueFunction, std::shared_ptr<const Model> model);
+  std::pair<double, double> GetOptimalControl(std::shared_ptr<const State> state, std::shared_ptr<const ValueFunction> nextValueFunction);
 
   // solves Bellman's equation
   void Solve(std::shared_ptr<const State> prior);
