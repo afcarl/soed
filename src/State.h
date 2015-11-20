b9d3cc6cdb1da28670296135316fbf8577b19763
Chi Feng <email.chi.feng@gmail.com>
2015-11-20 18:43:43 -0500

    Update State.h

--- src/State.h
+++ src/State.h
@@ -1,16 +1,28 @@
 #ifndef State_h
 #define State_h
 
+#include <tuple>
+#include <vector>
+#include <memory>
+
+#include <Eigen/Core>
+
+#include "Model.h"
+
 class State
 {
 
 public:
 
   virtual double GetMean() const;
+  
   virtual double GetVariance() const;
+  
   virtual std::pair<double, double> GetMoments() const;
+  
   virtual double GetKL(std::shared_ptr<State> other) const;
-  virtual std::shared_ptr<State> GetNextState(std::shared_ptr<Model>, const double control, const double disturbance);
+  
+  virtual std::shared_ptr<State> GetNextState(std::shared_ptr<const Model>, const double control, const double disturbance) const;
 
 };
 
