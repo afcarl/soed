e97c58d2666c2f7660f021a2bd973158034cf6f9
Chi Feng <email.chi.feng@gmail.com>
2015-11-21 09:53:52 -0500

    Create RandomGenerator.h

---
+++ src/RandomGenerator.h
@@ -0,0 +1,38 @@
+#ifndef RandomGenerator_h
+#define RandomGenerator_h
+
+#include <random>
+
+class RandomGenerator
+{
+  public:
+
+    static std::mt19937 engine;
+    static bool isInitialized;
+    std::normal_distribution<double> normalDist;
+    std::uniform_real_distribution<double> uniformDist;
+
+    inline static void Initialize()
+    {
+      if (!RandomGenerator::isInitialized) {
+        std::random_device random;
+        RandomGenerator::engine = std::mt19937(random());
+        RandomGenerator::isInitialized = true;
+      }
+    }
+
+    inline static double GetNormal()
+    {
+      RandomGenerator::Initialize();
+      return RandomGenerator::normalDist(RandomGenerator::engine);
+    }
+
+    inline static double GetUniform()
+    {
+      RandomGenerator::Initialize();
+      return RandomGenerator::uniformDist(RandomGenerator::engine);
+    }
+    
+};
+
+#endif // ifndef RandomGenerator_h
