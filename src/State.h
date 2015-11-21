943af0dfb6358f498e5ca0ec05d6753cf619b207
Chi Feng <email.chi.feng@gmail.com>
2015-11-21 15:08:30 -0500

    Update State.h

--- src/State.h
+++ src/State.h
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <vector>
 #include <memory>
+#include <numeric>
 
 #include <Eigen/Core>
 
@@ -14,13 +15,31 @@ class State
 
 public:
 
-  virtual double GetMean();
+  int numParticles;
+  std::vector<double> particles;
+  std::vector<double> logWeights;
   
-  virtual double GetVariance();
+  State(const int numParticles) 
   
-  virtual std::pair<double, double> GetMoments();
+  inline std::pair<double, double> GetMoments()
+  {
+    double mean = std::accumulate(particles.begin(), particles.end(), 0.0) / numParticles;
+    double variance = std::inner_product(particles.begin(), particles.end(), particles.begin(), 0.0) / numParticles - mean * mean;
+    return std::make_pair(mean, variance);
+  }
   
-  virtual double GetKL(std::shared_ptr<State> other);
+  inline double GetKL(std::shared_ptr<const State> other)
+  {
+    auto moments = GetMoments();
+    double mu_1 = moments.first;
+    double sigma_1 = sqrt(moments.second);
+    
+    auto otherMoments = other->GetMoments();
+    double mu_2 = otherMoments.first;
+    double sigma_2 = sqrt(otherMoments.second);
+    
+    return log(sigma_2) - log(sigma_1) + (sigma_1 * sigma_1 + (mu_1 - mu_2) * (mu_1 - mu_2)) / (2 * sigma_2 * sigma_2) - 0.5;
+  }
   
   virtual std::shared_ptr<State> GetNextState(std::shared_ptr<const Model>, const double control, const double disturbance);
 
