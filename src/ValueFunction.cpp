aa0c18f2ac4a0ab7dca6c265b919432678e6c76b
Chi Feng <email.chi.feng@gmail.com>
2015-11-24 17:01:35 -0500

    Create ValueFunction.cpp

---
+++ src/ValueFunction.cpp
@@ -0,0 +1,15 @@
+#include "ValueFunction.h"
+
+using namespace std;
+using namespace Eigen;
+
+double ValueFunction::Evaluate(shared_ptr<const State> state)
+{
+
+}
+
+void ValueFunction::Train(const vector<shared_ptr<const State>> states, const VectorXd& costs)
+{
+
+}
+
