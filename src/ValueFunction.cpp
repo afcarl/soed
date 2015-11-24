cf51f49e08ffcba5ade6b1e995160696ca37f6de
Chi Feng <email.chi.feng@gmail.com>
2015-11-24 17:18:54 -0500

    Update ValueFunction.cpp

--- src/ValueFunction.cpp
+++ src/ValueFunction.cpp
@@ -16,14 +16,11 @@ double ValueFunction::Evaluate(shared_ptr<const State> state)
 
 void ValueFunction::Train(const vector<shared_ptr<const State>> states, const VectorXd& costs)
 {
-  int m = static_cast<int>(states.size());
-  int n = static_cast<int>(basisFunctions.size());
-  MatrixXd X(m, n);
-  for (int i = 0; i < m; ++i) {
+  MatrixXd X(states.size(), basisFunctions.size());
+  for (size_t i = 0; i < states.size(); ++i) {
     auto moments = states[i]->GetMoments();
-    for (int j = 0; j < n; ++j) {
+    for (size_t j = 0; j < basisFunctions.size(); ++j)
       X(i, j) = basisFunctions[j](moments.first, moments.second);
-    }
   }
   coefficients = (X.transpose() * X).ldlt().solve(X.transpose() * costs);
 }
