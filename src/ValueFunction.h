aa072aae1a2b18f079cab35e9c1bc5ce85f53d1e
Chi Feng <email.chi.feng@gmail.com>
2015-11-24 21:53:56 -0500

    Update ValueFunction.h

--- src/ValueFunction.h
+++ src/ValueFunction.h
@@ -12,14 +12,17 @@
 
 class ValueFunction
 {
-
-public:
+  
+private:
 
   // Collection of basis functions that take the first two moments from State::GetMoments()
   static std::vector<std::function<double(double, double)>> basisFunctions;
 
   Eigen::VectorXd coefficients;
+  
+public:
 
+  inline Eigen::VectorXd GetCoefficients() { return coefficients; }
   virtual double Evaluate(std::shared_ptr<const State> state);
   virtual void Train(const std::vector<std::shared_ptr<const State>> states, const Eigen::VectorXd& costs);
 
