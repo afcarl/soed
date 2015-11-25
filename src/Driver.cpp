cc6cf7e9481232c2414646427ec62dc0e6d3efc1
Chi Feng <email.chi.feng@gmail.com>
2015-11-24 20:05:40 -0500

    Update Driver.cpp

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -1,32 +1,60 @@
-#include <stuff>
+#include <memory>
+#include <vector>
+
+#include "MossbauerModel.h"
+#include "State.h"
+#include "DPSolver.h"
+
+using namespace std;
+using namespace Eigen;
 
 int main(int argc, char** argv) {
 
-  int stages = 3;
+  int numStages          = GetOption<int>(argc, argv, "-numStages", 3);
+  int numTrainingSamples = GetOption<int>(argc, argv, "-numTrainingSamples", 100);
+  int numParticles       = GetOption<int>(argc, argv, "-numParticles", 1000);
+  int numGridpoints      = GetOption<int>(argc, argv, "-numGridpoints", 21);
   
-  auto model = make_shared<MossbauerExperiment>();
+  double priorMean       = GetOption<double>(argc, argv, "-priorMean", 0.0);
+  double priorVariance   = GetOption<double>(argc, argv, "-priorVariance", 1.0);
+  double noiseVariance   = GetOption<double>(argc, argv, "-noiseVariance", 0.01);
   
-  auto solver = make_shared<DPSolver>(stages, model);
+  double trueTheta       = GetOption<double>(argc, argv "-trueTheta", 0.5);
   
-  vector<shared_ptr<State>> states(stages);
-  vector<double> controls(stages);
-  vector<double> costToGo(stages);
-  vector<double> disturbances(stages);
+  auto model = make_shared<MossbauerModel>();
+  model->SetPriorMean(priorMean);
+  model->SetPriorVariance(priorVariance);
+  model->SetNoiseVariance(noiseVariance);
+
+  auto solver = make_shared<DPSolver>();
+  solver->SetNumStages(numStages);
+  solver->SetModel(model);
+  solver->SetNumTrainingSamples(numTrainingSamples);
+  solver->SetNumGridpoints(numGridpoints);
   
-  states[0] = model->GetPriorState();
+  vector<shared_ptr<State>> states(numStages);
+  vector<double> controls(numStages);
+  vector<double> costsToGo(numStages);
+  vector<double> disturbances(numStages);
   
+  // create prior State
+  auto prior = make_shared<State>();
+  for (int i = 0; i < numParticles; ++i)
+    prior->AddParticle(model->GetPriorSample());
+  prior->Normalize();
+  
+  states[0] = prior;
+  
+  // compute optimal policy
   solver->Solve(states[0]);
   
-  // now we execute the policy on some data
-  
-  double trueTheta = 0.5;
-  
-  for (int k = 0; k < stages - 1; ++k) {
+  // now execute the policy on some data generated using trueTheta
+  for (int k = 0; k < numStages - 1; ++k) {
     auto controlPair = solver->GetOptimalControl(states[k]);
-    controls[k] = controlPair.first;
-    costToGo[k] = controlPair.second;
-    disturbances[k] = model->GetDisturbance(trueTheta, controls[k]);
-    states[k + 1] = states[k]->GetNextState(model, controls[k], disturbances[k]); 
+    controls[k]      = controlPair.first;
+    costsToGo[k]     = controlPair.second;
+    disturbances[k]  = model->GetDisturbance(trueTheta, controls[k]);
+    states[k + 1]    = states[k]->GetNextState(model, controls[k], disturbances[k]); 
   }
   
   return 0;
