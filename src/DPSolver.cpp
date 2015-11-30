2c77a572bbe390cd87e66f88537eb890d068f523
Chi Feng <email.chi.feng@gmail.com>
2015-11-30 14:28:15 -0500

    Update DPSolver.cpp

--- src/DPSolver.cpp
+++ src/DPSolver.cpp
@@ -1,26 +1,36 @@
-
+#include <Eigen/Core>
 #include "DPSolver.h"
 
-// solver initializes valueFunctions, making sure the last element is of type TerminalValueFunction
-DPSolver::DPSolver(const int stages, std::shared_ptr<const Model> model)
-{
-  
-}
+using namespace std;
+using namespace Eigen;
 
 // returns optimal control and value 
-std::pair<double, double> GetOptimalControl(std::shared_ptr<const State> state, std::shared_ptr<const ValueFunction> nextValueFunction)
+pair<double, double> GetOptimalControl(shared_ptr<const State> state, shared_ptr<const ValueFunction> nextValueFunction)
 {
-
+  // enumerate all controls and values on a grid
+  VectorXd controls = VectorXd::LinSpaced(-1, 1, numGridpoints); 
+  VectorXd values = VectorXd::Zeros(numGridpoints); 
+  for (int i = 0; i < numGridpoints; ++i) {
+    // WHAT IS THETA??
+    auto disturbance = model->GetDisturbance(theta, controls(i));
+    auto nextState = state->GetNextState(model, controls(i), disturbance);
+    values(i) = nextValueFunction->Evaluate(nextState);
+  }
+  // return optimal control and value among enumerated values 
+  VectorXd::Index optimalIndex;
+  double optimalValue = values.maxCoeff(&optimalIndex);
+  double optimalControl = controls(optimalIndex);
+  return make_pair<double, double>(optimalControl, optimalValue);
 }
 
 // solves Bellman's equation
-void Solve(std::shared_ptr<const State> prior)
+void Solve(shared_ptr<const State> prior)
 {
   // reset value functions
   valueFunctions.resize(numStages);
   for (int i = 0; i < numStages - 1; ++i)
-    valueFunctions[i] = std::make_shared<ValueFunction>();
-  valueFunctions[numStages - 1] = std::make_shared<TerminalValueFunction>(prior);
+    valueFunctions[i] = make_shared<ValueFunction>();
+  valueFunctions[numStages - 1] = make_shared<TerminalValueFunction>(prior);
   
   ComputeTrainingPoints();
   TrainValueFunctions();
