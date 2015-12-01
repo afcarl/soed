bcc104693e6d0a39ea31a7354ab20b609175d269
Chi Feng <email.chi.feng@gmail.com>
2015-12-01 17:37:25 -0500

    Update MossbauerModel.h

--- src/MossbauerModel.h
+++ src/MossbauerModel.h
@@ -31,10 +31,20 @@ public:
     double output = Evaluate(theta, control);
     return -0.5 * log(2.0 * M_PI * noiseVariance) - (output - disturbance) * (output - disturbance) / (2.0 * noiseVariance);
   }
-  
+
+  inline double GetNoiseSample() override
+  {
+    return RandomGenerator::GetNormal() * sqrt(noiseVariance);
+  }
+    
   inline double GetDisturbance(double theta, double control) override
   {
-    return Evaluate(theta, control) + RandomGenerator::GetNormal() * sqrt(noiseVariance);
+    return Evaluate(theta, control) + GetNoiseSample();
+  }
+  
+  inline double GetDisturbance(double theta, double control, double noise) override 
+  {
+    return Evaluate(theta, control) + noise;
   }
   
   inline double GetPriorSample() override
